e99d0e2a67323789adf50bb1f5b9136688b78ca5
underdogg <drosbeds@xs4all.nl>
2015-08-16 22:45:29 +0200

    Committing empty config files, so that user can fill them

---
+++ app/config/production/application.empty.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+    'base_path' => '/',
+
+    'database'  => [
+        'adapter'  => 'Mysql',
+        'host'     => 'localhost',
+        'username' => '',
+        'password' => '',
+        'dbname'   => '',
+        'charset'  => 'utf8',
+    ],
+
+    'memcache'  => [
+        'host' => 'localhost',
+        'port' => 11211,
+    ],
+
+    'cache'     => 'file',
+    //'cache'     => 'memcache',
+];
